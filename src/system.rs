501883a23232a1d6eac6c02283cc8cf9d2f5dc65
Almale02 <gaspardaniel080716@gmail.com>
2023-07-30 23:10:07 +0200

    norelease: save

--- src/system.rs
+++ src/system.rs
@@ -1,12 +1,10 @@
-use bevy_rapier2d::na::clamp;
-use bevy_rapier2d::prelude::*;
-
-use bevy::transform::commands;
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use rand::prelude::*;
 
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+use bevy_rapier2d::prelude::*;
+
 use crate::component::*;
-use crate::resource::*;
 
 pub fn init_rendering(
     mut commands: Commands
@@ -27,56 +25,3 @@ pub fn move_camera(
     set.p0().single_mut().translation.y = set.p1().single().translation.y; 
 }
 
-pub fn move_ball(
-    mut q_movement: Query<(&mut Velocity, &mut KinematicCharacterController, &KinematicCharacterControllerOutput), With<Ball>>,
-    mut q_transform: Query<&mut Transform, With<Ball>>,
-    keyboard: Res<Input<KeyCode>>,
-    time: Res<Time>,
-) {
-    let move_speed = 3.5;
-    let fall_speed = 2.5;
-
-    for (mut y_vel, mut controller, controller_info) in q_movement.iter_mut() {
-        let mut x_vel = 0.;
-        if keyboard.pressed(KeyCode::Right) {x_vel = move_speed * time.delta_seconds() * 100.}
-        if keyboard.pressed(KeyCode::Left) {x_vel = -move_speed * time.delta_seconds() * 100.}
-
-        if controller_info.grounded {
-            if keyboard.just_pressed(KeyCode::Space) { y_vel.linvel.y += 18. * (fall_speed / 2.)}
-        }
-
-        y_vel.linvel.y -= fall_speed;
-        if y_vel.linvel.y < -fall_speed {y_vel.linvel.y = -fall_speed}
-
-        controller.translation = Some(Vect {x: x_vel, y: y_vel.linvel.y});
-        
-        for mut transform in q_transform.iter_mut() {
-            transform.rotate_z(x_vel);
-        }
-    }
-}
-
-pub fn spawn_ball(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>
-    
-) {
-    commands.spawn((
-        Ball,
-        SpriteBundle {
-            texture: asset_server.load("player.png"),
-            transform: Transform::from_scale(
-                Vec3 {x: 2., y: 2., z: 0.}
-            ).with_translation(
-                Vec3 {x: 0., y: 360., z: 0.}
-            ),
-            ..default()
-        },
-
-        //TransformBundle::from(Transform::from_xyz(0., 300., 0.)),
-        Collider::ball(16. /2. * 2./3.),
-        KinematicCharacterController::default(),
-        KinematicCharacterControllerOutput::default(),
-        Velocity::zero()
-    ));
-}
---
+++ src/world_load/system.rs
@@ -0,0 +1,52 @@
+use std::sync::{Arc, Mutex};
+
+use bevy::prelude::*;
+
+use bevy_rapier2d::prelude::*;
+
+use crate::lib::USVec2::USVec2;
+use crate::resource::{
+    level_data::LevelData,
+    block_texture::BlockTexture
+};
+
+
+pub fn load_world(
+    mut commands: Commands,
+    mut level_data: ResMut<LevelData>, 
+    asset_server: Res<AssetServer>,
+    block_texture: Res<BlockTexture>,
+) {
+    let mut cmd = Arc::new(Mutex::new(commands));
+    
+    level_data.loop_block_grid(|x, y, block, _| {
+        let mut cmd = cmd.lock().unwrap();
+        let global_pos = level_data.grid_to_global_space_unit(
+            USVec2 {x, y}
+        );
+        if *block == 0 {return}
+
+        let texture = block_texture.texture_map.get(block);
+        cmd.spawn((
+            SpriteBundle {
+                texture: asset_server.load(texture.unwrap()),
+                transform: Transform::from_translation(
+                    Vec3::new(
+                        global_pos.x as f32 *level_data.grid_unit as f32,
+                        global_pos.y as f32*level_data.grid_unit as f32,
+                        0.
+                    )
+                ).with_scale(
+                    Vec3 {x: 2., y: 2., z: 0.}
+                ),
+                ..default()
+            },
+            RigidBody::Fixed,
+            Velocity {
+                linvel: Vec2::new(1.0, 5.0),
+                angvel: 0.4
+            },
+            Collider::cuboid(8., 8.),
+        ));
+    })
+}
